bacf2302cca3922e9bb4540023564e3c70f073e9
Jamie Ho <jamie.ho@airbnb.com>
2016-05-19 20:53:53 -0700

    Configuration HTTP call belongs in MovieActivity, not BaseFragment.

--- app/src/main/java/com/codepath/flickster/util/FlicksterConstants.java
+++ app/src/main/java/com/codepath/flickster/util/FlicksterConstants.java
@@ -6,4 +6,14 @@ public class FlicksterConstants {
     public static final String POSTER_IMAGE_SIZE = "w500";
     public static final String BACK_DROP_IMAGE_SIZE = "w1280";
     public static final double POSTER_SCREEN_WIDTH_FACTOR = 0.42;
+
+    private static String imageBaseURL;
+
+    public static String getImageBaseURL() {
+        return imageBaseURL;
+    }
+
+    public static void setImageBaseURL(String imageBaseURL) {
+        FlicksterConstants.imageBaseURL = imageBaseURL;
+    }
 }
